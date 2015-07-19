28d575a1e501eb06bc6273fdb22cef403a7ea8e9
Johnny Romero Rojas <johromro@gmail.com>
2015-07-18 22:22:21 -0600

    Vistas parciales y marco

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,12 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 
-
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
-router.get('/quizes/question',quiz_controller.question);
-router.get('/quizes/answer',quiz_controller.answer);
+router.get('/quizes/question', quizController.question);
+router.get('/quizes/answer',   quizController.answer);
 
 module.exports = router;
